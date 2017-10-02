95a2396f15135470c5828ec1e38c3b9ad4d88a65
Peter Staev <peter@tangrasoft.com>
2017-10-02 23:58:29 +0300

    fix webpack demo

--- demo/app/bundle-config.ts
+++ demo/app/bundle-config.ts
@@ -2,6 +2,8 @@ if ((<any>global).TNS_WEBPACK) {
     //registers tns-core-modules UI framework modules
     require("bundle-entry-points");
 
+    global.registerModule("nativescript-masked-text-field", () => require("nativescript-masked-text-field"));
+    
     //register application modules
     global.registerModule("main-page", () => require("./main-page"));
 }
